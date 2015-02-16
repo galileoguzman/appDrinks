5c35b21bd74dade7aae8aa678d9545a029519698
Galileo Guzmán <galileoguzman@gmail.com>
2015-02-16 14:30:55 -0600

    Initial Commit

---
+++ appDrinksTests/appDrinksTests.m
@@ -0,0 +1,40 @@
+//
+//  appDrinksTests.m
+//  appDrinksTests
+//
+//  Created by Galileo Guzman on 16/02/15.
+//  Copyright (c) 2015 Galileo Guzman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface appDrinksTests : XCTestCase
+
+@end
+
+@implementation appDrinksTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
